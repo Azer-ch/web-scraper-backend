5b2f40ac48dfa3e6bf15b2ac4a6afded122be92b
azer <azerchabbar@gmail.com>
2025-07-10 11:54:29 +0200

    chore: allow cors

--- main.go
+++ main.go
@@ -1,15 +1,33 @@
 package main
 
 import (
+	"log"
+	"net/http"
+
+	"os"
+
 	"github.com/Azer-ch/web-scraper/routes"
 	"github.com/joho/godotenv"
-	"log"
+	"github.com/rs/cors"
 )
 
 func main() {
 	if err := godotenv.Load(); err != nil {
 		log.Println("No .env file found or error loading .env file")
 	}
-	r := routes.SetupRouter()
-	r.Run(":8080")
+	handler := routes.SetupRouter()
+
+	allowedOrigin := os.Getenv("CORS_ALLOWED_ORIGIN")
+	if allowedOrigin == "" {
+		allowedOrigin = "http://localhost:3000"
+	}
+
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{allowedOrigin},
+		AllowedMethods:   []string{"GET", "POST", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		AllowCredentials: true,
+	})
+
+	http.ListenAndServe(":8080", c.Handler(handler))
 }
